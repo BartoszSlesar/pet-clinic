67830e56139147997db76817052072818c8bb257
Bartosz <Bartosz>
2022-10-17 18:58:29 +0200

    Added Controller and index page for owners. closes #13

--- pet-clinic-web/src/main/java/pl/com/bard/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/pl/com/bard/petclinic/controllers/OwnerController.java
@@ -3,12 +3,12 @@ package pl.com.bard.petclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/vets")
+@RequestMapping("/owners")
 @Controller
-public class VetController {
+public class OwnerController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String listVets() {
-        return "vets/index";
+        return "owners/index";
     }
 }
