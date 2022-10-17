ebc57871e1e4119801aa65cfb773cab38a8abd54
Bartosz <Bartosz>
2022-10-17 16:32:04 +0200

    Added CrudService for common methods, Refactored services for Owner,Pet,Vet. closes #11

--- pet-clinic-data/src/main/java/pl/com/bard/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/pl/com/bard/petclinic/services/PetService.java
@@ -1,15 +1,11 @@
 package pl.com.bard.petclinic.services;
 
 
+import pl.com.bard.petclinic.model.Owner;
 import pl.com.bard.petclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
+public interface PetService extends CrudService<Pet, Long> {
 
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/pl/com/bard/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/pl/com/bard/petclinic/services/VetService.java
@@ -1,15 +1,12 @@
 package pl.com.bard.petclinic.services;
 
 
+import pl.com.bard.petclinic.model.Pet;
 import pl.com.bard.petclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long> {
 
-    Vet findById(Long id);
 
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
