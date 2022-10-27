9f8389a9c36f3d628ed834c297e85cc274a6a27e
Bartosz <Bartosz>
2022-10-27 19:55:14 +0200

    added bootsrap data for pet clinic

--- pet-clinic-data/src/main/java/pl/com/bard/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/pl/com/bard/petclinic/services/map/PetServiceMap.java
@@ -2,11 +2,11 @@ package pl.com.bard.petclinic.services.map;
 
 
 import pl.com.bard.petclinic.model.Pet;
-import pl.com.bard.petclinic.services.CrudService;
+import pl.com.bard.petclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
--- pet-clinic-data/src/main/java/pl/com/bard/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/pl/com/bard/petclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package pl.com.bard.petclinic.services.map;
 
 import pl.com.bard.petclinic.model.Vet;
-import pl.com.bard.petclinic.services.CrudService;
+import pl.com.bard.petclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
     public Set<Vet> findAll() {
