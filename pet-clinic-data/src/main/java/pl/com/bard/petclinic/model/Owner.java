2d417f785eeaeab07e6354b5fd148ec53e8820b0
Bartosz <Bartosz>
2022-11-05 19:26:30 +0100

    Added Visit Entity update owner entity. closes #26

--- pet-clinic-data/src/main/java/pl/com/bard/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/pl/com/bard/petclinic/model/Owner.java
@@ -1,5 +1,18 @@
 package pl.com.bard.petclinic.model;
 
+import java.util.Set;
+
 /**/
 public class Owner extends Person {
+    private Set<Pet> pets;
+
+
+
+    public Set<Pet> getPet() {
+        return pets;
+    }
+
+    public void setPet(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
