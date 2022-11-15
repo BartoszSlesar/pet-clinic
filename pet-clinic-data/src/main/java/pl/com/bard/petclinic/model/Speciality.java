5de9b5c20edc9858f41876ef0b59a1b05505739a
Bartosz <Bartosz>
2022-11-15 21:32:07 +0100

    updated JPA for Vet and specialty. closes #37

--- pet-clinic-data/src/main/java/pl/com/bard/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/pl/com/bard/petclinic/model/Speciality.java
@@ -1,7 +1,19 @@
 package pl.com.bard.petclinic.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
