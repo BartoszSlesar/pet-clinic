034b350322326a66f286747d200763fb0abc73b0
Bartosz <Bartosz>
2022-11-15 21:38:42 +0100

    updated JPA for Pet and Visit. closes #38

--- pet-clinic-data/src/main/java/pl/com/bard/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/pl/com/bard/petclinic/model/Pet.java
@@ -2,6 +2,8 @@ package pl.com.bard.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Entity
@@ -22,6 +24,11 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getPetName() {
         return petName;
@@ -53,5 +60,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
