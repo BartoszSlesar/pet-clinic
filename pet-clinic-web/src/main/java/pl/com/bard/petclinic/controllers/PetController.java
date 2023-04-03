5c84effc377f3e94a5be2b08e01c5a934bfca62d
Bartosz <Bartosz>
2023-04-03 16:12:12 +0200

    added Model Attributes for PetController. #56

---
+++ pet-clinic-web/src/main/java/pl/com/bard/petclinic/controllers/PetController.java
@@ -0,0 +1,50 @@
+package pl.com.bard.petclinic.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.com.bard.petclinic.model.Owner;
+import pl.com.bard.petclinic.model.PetType;
+import pl.com.bard.petclinic.services.OwnerService;
+import pl.com.bard.petclinic.services.PetService;
+import pl.com.bard.petclinic.services.PetTypeService;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping("/owners/{ownerId}")
+public class PetController {
+
+    private static final String VIEWS_PETS_CREATE_OR_UPDATE_FORM = "pets/createOrUpdatePetForm";
+
+    private final PetService petService;
+    private final OwnerService ownerService;
+    private final PetTypeService petTypeService;
+
+    public PetController(PetService petService, OwnerService ownerService, PetTypeService petTypeService) {
+        this.petService = petService;
+        this.ownerService = ownerService;
+        this.petTypeService = petTypeService;
+    }
+
+    @InitBinder("owner")
+    public void initOwnerBinder(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("types")
+    public Collection<PetType> populatePetTypes() {
+        return petTypeService.findAll();
+    }
+
+    @ModelAttribute("owner")
+    public Owner findOwner(@PathVariable Long ownerId) {
+        return ownerService.findById(ownerId);
+    }
+
+
+}
